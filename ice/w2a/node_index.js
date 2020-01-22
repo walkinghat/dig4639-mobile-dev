2ef137d1c716b23aef9cefb555c3d84fe313f8d0
SparkleRocket <sparklerocket@SparkleRockets-MacBook-Pro.local>
2020-01-22 09:53:29 -0500

    dj playing changes

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,7 +1,8 @@
 console.log("Hello World");
 var x = 1;
 var y = new Array();
-y[0] = 1;
+y[0] = 88;
+
 function scopeDemo(y1) {
     console.log(x)
     let a = 3, b = 4, c = 5;
@@ -10,12 +11,15 @@ function scopeDemo(y1) {
     console.log(c);
     console.log(y1);
     y1[0] = 500;
+    z = y1[0];
    
     var x = 2;
     if(true) {
         var x = 0;
     }
-    return a;
+    return z;
 }
 console.log(scopeDemo(y)+10);
+x = scopeDemo(y);  //argument is the new array
+console.log(z);
 
